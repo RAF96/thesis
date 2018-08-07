e2001bfe269b13986646ac45c62e93632dcb3db6
RAF96 <raf-96@ya.ru>
2018-08-07 11:34:05 +0300

    Delete the WriterPlot, The WriterPlot_new now is the WriterPlot

--- gui.py
+++ gui.py
@@ -2,7 +2,7 @@ import tkinter as tk
 from tkinter import Frame
 import tkinter.ttk as ttk
 import controller
-from writer_plot import WriterPlot_new
+from writer_plot import WriterPlot
 from view_models import DisplayedTime
 
 
@@ -194,7 +194,7 @@ class GuiPlot(tk.Frame):
 
         self.frame_for_writer_plot = tk.Frame(self)
         self.frame_for_writer_plot.pack(side = tk.TOP)
-        self.writer_plot = WriterPlot_new(self.frame_for_writer_plot, controller, self.time)
+        self.writer_plot = WriterPlot(self.frame_for_writer_plot, controller, self.time)
         self.writer_plot.pack(side = tk.TOP)
 
         self.button = tk.Button(self, text="Пауза/Продолжить", command=(lambda: self.writer_plot.pause_continue()))
--- writer_plot.py
+++ writer_plot.py
@@ -6,78 +6,8 @@ from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 from matplotlib.figure import Figure
 import tkinter as tk
 
-#to do. The part of code wiil delete after repair scale and add "changes end time"
-'''
+
 class WriterPlot(tk.Frame):
-
-    def __init__(self, parent, controller, time):
-        tk.Frame.__init__(self, parent)
-        self.parent = parent
-        self.controller = controller
-        self.time = time
-        self.fig = Figure()
-        self.canvas = FigureCanvasTkAgg(self.fig, master=parent)
-        self.canvas.get_tk_widget().pack()
-        self.ax = self.fig.add_subplot(111, xlim=(-10, 10), ylim=(-10, 10))
-        self.line, = self.ax.plot([], [], color='r')
-        self.ax.axhline(y=0, color='b')
-        self.ax.axvline(x=0, color='b')
-        self.animation = None
-
-    def str_to_float(self, x):
-        try:
-            res = float(x)
-            return res
-        except:
-            return None
-
-    #change make after click button "Запуск". it is not good
-    def update_xy_lim(self, bottom_x, bottom_y, up_x, up_y):
-        bottom_x = self.str_to_float(bottom_x) if self.str_to_float(bottom_x) is not None else self.ax.get_xlim()[0]
-        bottom_y = self.str_to_float(bottom_y) if self.str_to_float(bottom_y) is not None else self.ax.get_ylim()[0]
-        up_x = self.str_to_float(up_x) if self.str_to_float(up_x) is not None else self.ax.get_xlim()[1]
-        up_y = self.str_to_float(up_y) if self.str_to_float(up_y) is not None else self.ax.get_ylim()[1]
-
-        self.ax.set_xlim(bottom_x, up_x)
-        self.ax.set_ylim(bottom_y, up_y)
-
-    def get_init(self):
-        def init():
-            self.line.set_data([], [])
-            return self.line,
-        return init
-
-    def get_animate(self):
-        def animate(i):
-            x = numpy.linspace(self.plot_bottom_x, self.plot_up_x, self.num_x)
-            y = self.function(x, self.time.t.get())
-            self.time.next()
-            self.controller.new_function(self.time.t.get())
-            self.line.set_data(x, y)
-            return self.line,
-        return animate
-
-    def clear(self):
-        if self.animation is not None:
-            self.animation.event_source.stop()
-
-    def print_animation(self, function, plot_bottom_x, plot_up_x):
-        self.clear()
-        self.time.t.set(0.1)
-        self.time.dt = 0.05
-
-        self.max_delta_x = self.controller.get_max_delta_x()
-        self.plot_bottom_x, self.plot_up_x = plot_bottom_x, plot_up_x
-        self.num_x = int((self.plot_up_x - self.plot_bottom_x) / self.max_delta_x)
-
-        self.function = function
-        self.animation = animation.FuncAnimation(self.fig, self.get_animate(), init_func=self.get_init(),
-                                       frames=200, interval=20, blit=True)
-
-        self.parent.mainloop()
-'''
-
-class WriterPlot_new(tk.Frame):
     def __init__(self, parent, controller, time):
         tk.Frame.__init__(self, parent)
         self.parent = parent
