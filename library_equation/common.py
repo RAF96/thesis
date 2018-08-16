d7e608f895475155aac2f930ff4ae1135e374246
RAF96 <raf-96@ya.ru>
2018-08-16 15:05:43 +0300

    Solve a problem with example 2.2

--- library_equation/common.py
+++ library_equation/common.py
@@ -1,5 +1,10 @@
 import sympy
 from sympy import abc
+import numpy
 
 def calculate(function):
-    return sympy.lambdify((abc.x, abc.t), function)
+    return sympy.lambdify((abc.x, abc.t), function) # , modules=["math", "mpmath", "sympy", {"Integral": integral_as_quad}])
+
+def integral_as_quad(expr, lims):
+    var, a, b = lims
+    return scipy.integrate.quad(lambdify(var, expr), a, b)[0]
--- library_equation/heat_equation.py
+++ library_equation/heat_equation.py
@@ -36,8 +36,9 @@ def gui_main_one_dimensional__heat_equation__homogeneous(animation_plot, coef, y
     return animation_plot
 
 
-def gui_main_one_dimensional__heat_equation__inhomogeneous(coef, y__x_tzero, external_influences):
-    return calculate__one_dimensional__heat_equation__inhomogeneous(coef, y__x_tzero, external_influences)
+def gui_main_one_dimensional__heat_equation__inhomogeneous(animation_plot, coef, y__x_tzero, external_influences):
+    calculate__one_dimensional__heat_equation__cauchy(animation_plot, coef, y__x_tzero, external_influences)
+    return animation_plot
 
 def one_dimensional__heat_equation__homogeneous(coef, y__x_tzero):
     sqrt_coef = coef ** 0.5
@@ -51,32 +52,6 @@ def one_dimensional__heat_equation__homogeneous(coef, y__x_tzero):
     res = res.subs(t, sympy.abc.t)
     return res
 
-#to do. Too slowly and I didn't decide what I need to do. Supporting was stoped. Now not working.
-def calculate__one_dimensional__heat_equation__inhomogeneous(coef, y__x_tzero, external_influences):
-    first_augend = one_dimensional__heat_equation__homogeneous(coef, y__x_tzero)
-    lambdify_first_augend = sympy.lambdify((abc.x, abc.t), first_augend)
-
-    def function(x_val, t_val):
-        x, L = sympy.symbols("x L")
-        t, S = sympy.symbols("t S")
-        sqrt_coef = coef ** 0.5
-        new_external_influences = external_influences.subs([(x, L), (abc.t, S)])
-        function_for_integral = 1 / (2 * sqrt_coef * (sympy.pi * (t - S)) ** 0.5) * \
-                                new_external_influences * \
-                                sympy.exp(-(x - L) ** 2 / (4 * coef * (t - S)))
-
-
-        lambdify_function_for_integral = sympy.lambdify((S, L, t, x), function_for_integral)
-
-        second_augend, err = scipy.integrate.dblquad(lambdify_function_for_integral,
-                                                     -numpy.inf, numpy.inf,
-                                                     lambda S: 0, lambda S: t_val,
-                                                     args=(t_val, x_val),
-                                                     epsabs=0.000001, epsrel=0.000001)
-        return second_augend + lambdify_first_augend(x_val, t_val)
-
-    res = numpy.vectorize(function)
-    return res
 
 def gui_main_one_dimensional__heat_equation__task_1(animation_plot, input_data):
     input_data_get = input_data.get("coef", "y__x_tzero", "external_influences", "y__xzero_t", "y__xeql_t")
@@ -137,12 +112,51 @@ def calculate__one_dimensional__heat_equation__boundary_task(animation_plot, coe
                              gamma2 * (y[i + 1] - 2 * y[i] + y[i - 1]) + \
                              dt * external_influences(x[i], t)
 
-        # y_next[0] = (third_boundary_function__xzero(x[0], t) * dt + y[0]) / \
-        #        (dt * a__xzero + b__xzero)
-        # y_next[n] = (third_boundary_function__xeql(x[n], t) * dt + y[n]) / (dt * a__xeql + b__xeql)
-
         y_next[0] = boundary_function__xzero(x[0], t, dt, y[0])
         y_next[n] = boundary_function__xeql(x[n], t, dt, y[n])
+
+        y = y_next
+        return y_next
+
+    animation_plot.clean_result_part()
+    y = None
+    for t in animation_plot.get_t():
+        y = function(animation_plot.get_x(), t, y)
+        animation_plot.y.append(y)
+
+    return animation_plot
+
+
+def calculate__one_dimensional__heat_equation__cauchy(animation_plot, coef, y__x_tzero, external_influences):
+
+    y__x_tzero = calculate(y__x_tzero)
+    external_influences = calculate(external_influences)
+    sqrt_coef = coef ** 0.5
+
+    def function(x, t, y):
+        y_next = None
+        n = len(x) - 1
+        dx = x[1] - x[0]
+        dt = 0.05
+        gamma2 = dt * (1. / dx * sqrt_coef) ** 2
+
+        if y is None:
+            y = [0] * (n + 1)
+            for i in range(0, n+1):
+                y[i] = y__x_tzero(x[i], t) + dt * external_influences(x[i], t)
+            return y
+
+        y_next = [0] * (n + 1)
+        for i in range(1, n):
+            y_next[i] = y[i] + \
+                             gamma2 * (y[i + 1] - 2 * y[i] + y[i - 1]) + \
+                             dt * external_influences(x[i], t)
+
+        i = 0
+        y_next[0] = y_next[1]
+
+        i = n
+        y_next[n] = y_next[n-1]
 
         y = y_next
         return y_next
